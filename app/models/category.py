afce91e4dcd70148ed5e9b1e6f3a57d1de061f7c
l3nik <l3nik72@gmail.com>
2012-11-18 23:04:03 +0100

    category

--- app/models/category.py
+++ app/models/category.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Category(models.Model):
-    
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
     
     class Meta:
         app_label= 'app'
