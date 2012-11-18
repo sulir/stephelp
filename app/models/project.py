5a0572b2f75b64d94d86db445bd81db7102e9d91
l3nik <l3nik72@gmail.com>
2012-11-18 23:13:19 +0100

    zmeny

--- app/models/project.py
+++ app/models/project.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Project(models.Model):
-    
+    Name = models.CharField(max_length=100)
+    Description = models.TextField()
     
     class Meta:
         app_label= 'app'
