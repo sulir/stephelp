18a528982afd5b7c1e7927990de3af7f77177101
rebrohead <rebrohead@gmail.com>
2012-12-02 16:10:47 +0100

    Modified methods

--- app/models/project.py
+++ app/models/project.py
@@ -1,15 +1,30 @@
 from django.db import models
+from django.db.models.aggregates import Avg
 from user import User
 from category import Category
 
+class ProjectManager(models.Manager):
+    def by_top_users(self, count):
+        return self.order_by('-owner__points')[:count]
+    
+    def by_category(self, category_id):
+        return self.filter(category=category_id).order_by('-owner__points')
+
 class Project(models.Model):
-    owner = models.ForeignKey(User)
-    category = models.ForeignKey(Category)
+    owner = models.ForeignKey(User, related_name='projects')
+    category = models.ForeignKey(Category, related_name='projects')
     name = models.CharField(max_length=100)
     description = models.TextField()
     
+    objects = ProjectManager()
+    
     def __unicode__(self):
         return self.name
+    
+    @property
+    def completion(self):
+        parts_pct = [part.completion for part in self.parts.all()]
+        return sum(parts_pct) / len(parts_pct)
     
     class Meta:
         app_label= 'app'
--- app/models/project_part.py
+++ app/models/project_part.py
@@ -2,13 +2,30 @@ from django.db import models
 from project import Project
 from user import User
 
+PLANNED, LAUNCHED, FINISHED = 'P', 'L', 'F'
+STATUS = (
+    (PLANNED, "planned"),
+    (LAUNCHED, "launched"),
+    (FINISHED, "finished")
+)
+
 class ProjectPart(models.Model):
-    project = models.ForeignKey(Project)
-    assigned_to = models.ForeignKey(User)
+    project = models.ForeignKey(Project, related_name='parts')
+    assigned_to = models.ForeignKey(User, null=True, related_name='parts')
     description = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS)
     
     def __unicode__(self):
         return self.description
+    
+    @property
+    def completion(self):
+        percentage = {PLANNED: 0, LAUNCHED: 50, FINISHED: 100}
+        return percentage[self.status]
+    
+    @property
+    def can_be_supported(self):
+        return (status == PLANNED) and (self.assigned_to != self.project.owner)
     
     class Meta:
         app_label= 'app'
