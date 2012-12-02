671c3381beeb12e8da13b3c17f660e4da8101827
rebrohead <rebrohead@gmail.com>
2012-12-02 16:20:16 +0100

    Update urls,views

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^projects/$', views.projects, name='projects'),
+    url(r'^projects/', views.projects, name='projects'),
+    url(r'^about/', views.about, name='about'),
     url(r'^login_view', views.login_view, name='login_view'),
     url(r'^logout_view', views.logout_view, name='logout_view'),
+    url(r'^userdetail/(?P<user_id>\d+)', views.user_detail, name='user_detail'),
 )
--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render_to_response
+from app.models import UserProfile, Project, ProjectPart
 
 def index(request):
     return render(request, 'app/index.html')
 
 def projects(request):
     return render(request, 'app/projects.html')
+
+def about(request):
+    return render(request, 'app/about.html')
 
 def login_view(request):
     username = request.POST['username']
@@ -22,3 +26,9 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'app/index.html') 
+
+def user_detail(request, user_id):
+    p = UserProfile.objects.get(pk=user_id)
+    pr= Project.objects.filter(owner=user_id)
+    prp= ProjectPart.objects.filter(assigned_to=user_id)
+    return render(request,'app/profile.html', {'userd': p,'projects':pr,'project_parts':prp})
