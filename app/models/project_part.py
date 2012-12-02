8c9359045d3fb4425dd9adc4503367b35e2d2309
Matúš Sulír <matus.sulir@gmail.com>
2012-12-02 15:19:42 +0100

    Add status field to ProjectPart

--- app/models/project_part.py
+++ app/models/project_part.py
@@ -2,10 +2,18 @@ from django.db import models
 from project import Project
 from user import User
 
+PLANNED, LAUNCHED, FINISHED = 'P', 'L', 'F'
+STATUS = (
+    (PLANNED, "planned"),
+    (LAUNCHED, "launched"),
+    (FINISHED, "finished")
+)
+
 class ProjectPart(models.Model):
-    project = models.ForeignKey(Project)
-    assigned_to = models.ForeignKey(User)
+    project = models.ForeignKey(Project, related_name='parts')
+    assigned_to = models.ForeignKey(User, null=True, related_name='parts')
     description = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS)
     
     def __unicode__(self):
         return self.description
